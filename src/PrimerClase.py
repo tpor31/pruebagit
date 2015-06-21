ad74f55c03dbaa3c7bb211fc33f71ecdffa746aa
Antonio Porcelli <porcelli.antonio@gmail.com>
2015-06-21 01:20:25 -0300

    comentarios

---
+++ src/PrimerClase.py
@@ -0,0 +1,18 @@
+'''
+Created on 21/6/2015
+
+@author: tony
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        print("Que suerte")
+
+
+c = MyClass()        
+        
---
+++ src/modulo.py
@@ -0,0 +1,21 @@
+'''
+Created on 31/3/2015
+
+@author: tony
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+    num = 0 
+
+    
+    def __init__(self, params):
+        if True:
+            print("Esto siempre es sierto")
+    
+        '''
+        Constructor
+        '''
+        
